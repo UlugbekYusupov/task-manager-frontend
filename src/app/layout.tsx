412cbbc50b45ab768e7d47fdb27007c26df2b67f
Ulugbek9602 <ulugbek.yusupov@hygge.software>
2025-03-03 02:43:53 +0500

    logout implemented

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
 import { Providers } from "../store/provider";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata: Metadata = {
   title: "Task Manager App",
--- src/components/dashboard/Navbar.tsx
+++ src/components/dashboard/Navbar.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { useState } from "react";
-import { Bell, HelpCircle, Search } from "lucide-react";
+import { Bell, HelpCircle, LogOut, Search } from "lucide-react";
 import Image from "next/image";
 import { DropdownItem } from "../ui/dropdown/DropdownItem";
-import Link from "next/link";
 import Input from "../ui/input/InputField";
+import Button from "../ui/button/Button";
+import { useRouter } from "next/navigation";
+import { useDispatch } from "react-redux";
+import { logout } from "@/store/slices/authSlice";
 
 const Navbar = () => {
   const [dropdownOpen, setDropdownOpen] = useState<string | null>(null);
@@ -13,6 +16,9 @@ const Navbar = () => {
   const toggleDropdown = (menu: string) => {
     setDropdownOpen(dropdownOpen === menu ? null : menu);
   };
+
+  const dispatch = useDispatch();
+  const router = useRouter();
 
   const DropdownButton = ({
     icon: Icon,
@@ -50,6 +56,11 @@ const Navbar = () => {
       <path fillRule="evenodd" clipRule="evenodd" d={path} fill="" />
     </svg>
   );
+
+  const handleLogout = () => {
+    dispatch(logout());
+    router.push("/auth/login");
+  };
 
   return (
     <nav className="w-full bg-white border-l border-b border-gray-200 p-4 flex justify-between items-center">
@@ -128,13 +139,16 @@ const Navbar = () => {
                   </li>
                 ))}
               </ul>
-              <Link
-                href="/auth/login"
-                className="flex items-center gap-3 px-3 py-2 mt-3 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
+
+              <Button
+                variant="outline"
+                startIcon={<LogOut />}
+                size="sm"
+                className="w-full mt-3 cursor-pointer text-red-500 "
+                onClick={handleLogout}
               >
-                <ProfileIcon path="M15.1007 19.247C14.6865 19.247 14.3507 18.9112 14.3507 18.497L14.3507 14.245H12.8507V18.497C12.8507 19.7396 13.8581 20.747 15.1007 20.747H18.5007C19.7434 20.747 20.7507 19.7396 20.7507 18.497L20.7507 5.49609C20.7507 4.25345 19.7433 3.24609 18.5007 3.24609H15.1007C13.8581 3.24609 12.8507 4.25345 12.8507 5.49609V9.74501L14.3507 9.74501V5.49609C14.3507 5.08188 14.6865 4.74609 15.1007 4.74609L18.5007 4.74609C18.9149 4.74609 19.2507 5.08188 19.2507 5.49609L19.2507 18.497C19.2507 18.9112 18.9149 19.247 18.5007 19.247H15.1007ZM3.25073 11.9984C3.25073 12.2144 3.34204 12.4091 3.48817 12.546L8.09483 17.1556C8.38763 17.4485 8.86251 17.4487 9.15549 17.1559C9.44848 16.8631 9.44863 16.3882 9.15583 16.0952L5.81116 12.7484L16.0007 12.7484C16.4149 12.7484 16.7507 12.4127 16.7507 11.9984C16.7507 11.5842 16.4149 11.2484 16.0007 11.2484L5.81528 11.2484L9.15585 7.90554C9.44864 7.61255 9.44847 7.13767 9.15547 6.84488C8.86248 6.55209 8.3876 6.55226 8.09481 6.84525L3.52309 11.4202C3.35673 11.5577 3.25073 11.7657 3.25073 11.9984Z" />
-                Sign out
-              </Link>
+                Logout
+              </Button>
             </div>
           )}
         </div>
